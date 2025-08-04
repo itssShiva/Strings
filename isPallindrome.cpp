a6aa889f7d8d5ad23d21da454b1d42484277f878
itssShiva <iitts.shiva@gmail.com>
2025-08-04 07:38:17 +0530

    Update isPallindrome.cpp

--- isPallindrome.cpp
+++ isPallindrome.cpp
@@ -1,21 +1,16 @@
 #include<iostream>
 #include<String>
 using namespace std;
-void reverseString(string &k){
-    int n=k.size();
+bool isPallindrome(string &s){
+    int n=s.size();
     for(int i=0;i<n/2;i++){
-       swap(k[i],k[n-i-1]);
+        if(s[i]!=s[n-i-1]) return false;
+        else return true;
     }
 }
-
-bool isPallindrome(string s,string k){
-    if(k==s)  return true;
-        else return false;
-}
 int main(){
-    string s="madam";
-    string original=s;
-    reverseString(s);
-   bool valid= isPallindrome(original,s);
+    string s="shiva";
+
+   bool valid= isPallindrome(s);
     cout<<(valid?"True":"False");
 }
