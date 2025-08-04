fd4e9ad49e27d9726c2167525469bab3f4546b53
itssShiva <iitts.shiva@gmail.com>
2025-08-04 08:11:55 +0530

    Update isPallindrome.cpp

--- isPallindrome.cpp
+++ isPallindrome.cpp
@@ -1,16 +1,41 @@
-#include<iostream>
-#include<String>
+#include <bits/stdc++.h>
 using namespace std;
-bool isPallindrome(string &s){
-    int n=s.size();
-    for(int i=0;i<n/2;i++){
-        if(s[i]!=s[n-i-1]) return false;
-        else return true;
+
+char toLowerCase(char ch) {
+    if (ch >= 'a' && ch <= 'z') {
+        return ch;
+    } else {
+        return ch - 'A' + 'a';
     }
 }
-int main(){
-    string s="shiva";
 
-   bool valid= isPallindrome(s);
-    cout<<(valid?"True":"False");
+bool checkPalindrome(string s) {
+    int n = s.size();
+    int low = 0;
+    int high = s.size() - 1;
+
+    while (low < high) {
+        while (low < high && !isalnum(s[low])) low++;
+        while (low < high && !isalnum(s[high])) high--;
+        if (toLowerCase(s[low]) != toLowerCase(s[high])) {
+            return false;
+        }
+        low++;
+        high--;
+    }
+    return true;
+}
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input); // allows spaces and punctuation
+
+    if (checkPalindrome(input)) {
+        cout << "Palindrome" << endl;
+    } else {
+        cout << "Not a palindrome" << endl;
+    }
+
+    return 0;
 }
